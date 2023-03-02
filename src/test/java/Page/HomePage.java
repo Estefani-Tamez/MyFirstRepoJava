22a91c211244aacd6ae85067740fa38beb3b66b3
EstefaniTamez <estefaniltj@hotmail.com>
2023-03-02 15:27:47 -0600

    Sesion Java clase anterior

--- src/test/java/Page/HomePage.java
+++ src/test/java/Page/HomePage.java
@@ -1,9 +1,19 @@
 package Page;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
+
+    protected ChromeDriver driver;
+
+    public HomePage(ChromeDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,  this);
+    }
+
     //@FindBy(xpath = "user")
     @FindBy(id = "first-name")
     public WebElement first_name;
